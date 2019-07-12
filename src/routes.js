1d0604796db4ae2a94efdaa9304aa12c5b5f5c3d
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-07-12 02:45:35 +0000

    feat: add token middleware and update user route

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,14 @@ import { Router } from 'express';
 import AuthController from './app/controllers/AuthController';
 import UserController from './app/controllers/UserController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/auth', AuthController.store);
+
+routes.use(authMiddleware);
 routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
 
 export default routes;
